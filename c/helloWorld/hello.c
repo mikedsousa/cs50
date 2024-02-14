8d5a9a72765ebe78e88d5cfd67882d365d3dda68
mikedsousa <95877997+mikedsousa@users.noreply.github.com>
2024-02-14 10:37:18 -0300

    att

---
+++ c/helloWorld/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, World!\n");
+
+    char answer[100];
+    printf("What's your name?\n");
+    fgets(answer, sizeof(answer), stdin);
+    printf("Hello, %s\n", answer);
+
+}
