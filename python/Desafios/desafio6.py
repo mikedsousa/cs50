0aa13cdeb727ae54ea260a9e7dd518373234825f
mikedsousa <95877997+mikedsousa@users.noreply.github.com>
2024-02-20 09:19:41 -0300

    Update desafio6.py

--- python/Desafios/desafio6.py
+++ python/Desafios/desafio6.py
@@ -1,7 +1,9 @@
 numero = int(input("Digite um número: "))
 
 dobro = numero * 2
+
 triplo = numero * 3
+
 raizQuadrada = numero ** (1/2)
 
 print("O dobro de {} é {}, o triplo é {} e a raiz quadrada é {}".format(numero, dobro, triplo, raizQuadrada))
