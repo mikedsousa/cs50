5a0987c646e9f6421100c6579bfba526d1810f7c
mikedsousa <95877997+mikedsousa@users.noreply.github.com>
2024-03-01 08:33:14 -0300

    Update cat.py

--- python/03_loops/cat.py
+++ python/03_loops/cat.py
@@ -1,4 +1,21 @@
-i = 0
-while i < 3:
-  print("meow")
-  i += 1
+# i = 0
+# while i < 3:
+#   print("meow")
+#   i += 1
+
+def main():
+  number = get_number()
+  meow(number)
+  
+def get_number():
+  while True:
+    n = int(input("What's x? "))
+    if n > 0:
+      break
+  return n
+  
+def meow(n):
+  for _ in range(n):
+    print("meow")
+  
+main()
