e767698ad8458d0f06abb316d01b183640c6fa72
mikedsousa <95877997+mikedsousa@users.noreply.github.com>
2024-03-04 07:52:24 -0300

    att

--- python/03_loops/09_hogwarts.py
+++ python/03_loops/09_hogwarts.py
@@ -1,8 +1,45 @@
+# students = {
+#   "Harry": "Gryffindor",
+#   "Hermione": "Gryffindor",
+#   "Ron": "Gryffindor",
+#   "Draco": "Slytherin",
+# }
 
-students = ["Hermione", "Harry", "Ron"]
+# for student in students:
+#   print(student, students[student], sep=" from house ")
+
+###################################
+
+# for student in students:
+#     print(f"Welcome to Hogwarts, {student}!")
+  
+# for i in range(len(students)):
+#   print(i + 1, students[i])
+
+###################################
+
+students = [
+  {
+    "name": "Harry",
+    "house": "Gryffindor",
+    "patronus": "Stag"
+  },
+  {
+    "name": "Hermione",
+    "house": "Gryffindor",
+    "patronus": "Otter"
+  },
+  {
+    "name": "Ron",
+    "house": "Gryffindor",
+    "patronus": "Jack Russell Terrier"
+  },
+  {
+    "name": "Draco",
+    "house": "Slytherin",
+    "patronus": None
+  },
+]
 
 for student in students:
-    print(f"Welcome to Hogwarts, {student}!")
-  
-for i in range(len(students)):
-  print(i + 1, students[i])
+  print(f"Welcome to Hogwarts, {student['name']} from house {student['house']}! Your patronus is a {student['patronus']}.")
